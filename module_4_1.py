82c8c32126a777a7edc32cec53d7818325aa624e
pinkklss <shaker_04@mail.ru>
2024-06-19 17:02:38 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as dave
+from true_math import divide as dive
+
+
+result1 = dave(69, 3)
+result2 = dave(3, 0)
+result3 = dive(49, 7)
+result4 = dive(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
